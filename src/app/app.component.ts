9831c42b1b7e902f6cbc8d7856413ccebf3de979
VENKATARAMAIAH <94680948+venkataramayya@users.noreply.github.com>
2025-02-25 12:46:02 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,14 @@
-import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { HomeComponent } from "./home/home.component";
+
 import { NavbarComponent } from "./navbar/navbar.component";
-import { HoursTrackedComponent } from "./hours-tracked/hours-tracked.component";
-import { TimelineComponent } from "./timeline/timeline.component";
-import { EmployeeLogsComponent } from "./employee-logs/employee-logs.component";
-import { ScreencastsComponent } from "./screencasts/screencasts.component";
-import { ProjectListComponent } from "./project-list/project-list.component";
-import { TaskProgressComponent } from './task-progrees/task-progrees.component';
-import { TicketListComponent } from './ticket-list/ticket-list.component';
-import { TimesheetComponent } from "./timesheet/timesheet.component";
-import { AttendanceComponent } from "./attendance/attendance.component";
+
 import { SidebarRespComponent } from "./sidebar-resp/sidebar-resp.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DashboardComponent, HomeComponent, NavbarComponent, HoursTrackedComponent, TimelineComponent, EmployeeLogsComponent, ScreencastsComponent, ProjectListComponent, EmployeeListComponent, TaskProgressComponent, TicketListComponent, TimesheetComponent, AttendanceComponent, SidebarRespComponent],
+  imports: [RouterOutlet, DashboardComponent, NavbarComponent,  SidebarRespComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
